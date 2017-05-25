f704f422c48173c65db446ffa879a6ace9dc3e13
Vikram Raman <vikram@wavefront.com>
2017-05-25 14:48:54 -0700

    Added launchd support

--- Formula/wfproxy.rb
+++ Formula/wfproxy.rb
@@ -3,7 +3,7 @@ require "formula"
 class Wfproxy < Formula
   homepage "https://www.wavefront.com"
   url "https://github.com/vikramraman/homebrew-wfproxy/raw/master/files/4.6.0/wfproxy-4.6.0.tar.gz"
-  sha256 "a3a0401bb68e1d17bc3fc56b3f0712ed9642dbdf89178a08d4cb3141d93ea8f1"
+  sha256 "91c44bfdccf2247cdc02326ea8739104c696a4a53ce709b12bb23e10e01a7e68"
 
   bottle :unneeded
 
@@ -12,5 +12,39 @@ class Wfproxy < Formula
   def install
 	lib.install "lib/proxy-uber.jar"
   	bin.install "bin/wfproxy"
+	etc.install "etc/wfproxy.conf"
+  end
+
+  plist_options :manual => "wfproxy -f #{HOMEBREW_PREFIX}/etc/wfproxy.conf"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <dict>
+          <key>SuccessfulExit</key>
+          <false/>
+        </dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/wfproxy</string>
+          <string>-f</string>
+          <string>#{etc}/wfproxy.conf</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{var}</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/wfproxy.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/wfproxy.log</string>
+      </dict>
+    </plist>
+    EOS
   end
 end
