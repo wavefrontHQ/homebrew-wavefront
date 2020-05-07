963f6f8c140e565ad7ed83ee856da03efbdb51d0
wf-jenkins <svc.wf-jenkins@vmware.com>
2020-05-07 10:36:47 -0700

    update Formula/wfproxy.rb for wfproxy-7.0.0.tar.gz

--- Formula/wfproxy.rb
+++ Formula/wfproxy.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Wfproxy < Formula
   homepage "https://www.wavefront.com"
-  url "http://wavefront-cdn.s3-website-us-west-2.amazonaws.com/brew/wfproxy-6.1.0.tar.gz"
-  sha256 "d22320bca44642b6654ed03f21bfc8819b416bcc8e2ddd164bcb47687f9aeb4a"
+  url "http://wavefront-cdn.s3-website-us-west-2.amazonaws.com/brew/wfproxy-7.0.0.tar.gz"
+  sha256 "db1acdd9164e13f3f5132047eebebcf7b8fffde078e4a9738aa5e0adcd75e90b"
 
   bottle :unneeded
 
