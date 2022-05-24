763c36ca0bb01fc59abd63a284c41028b1de56ab
German Laullon <glaullon@vmware.com>
2022-05-24 12:03:37 +0200

    updated wfproxy_new

--- Formula/wfproxy_new.rb
+++ Formula/wfproxy_new.rb
@@ -1,52 +1,28 @@
 class WfproxyNew < Formula
   homepage "https://www.wavefront.com"
-  url "https://wavefront-cdn-dev.s3.us-west-2.amazonaws.com/wavefront-proxy/wfproxy_11.2-SNAPSHOT_20220523-153903.zip"
-  sha256 "d95b60167323028c25b51152380d4a9a909fe0ac"
+  url "https://macos-testing-glaullon.s3.us-west-2.amazonaws.com/wfproxy_11.2-SNAPSHOT_20220524-115027.zip"
+  sha256 "29bcf4d0db15caca6e734f00f0cf8f7a479f4ea0c0830372210a0a5e4ab381da"
 
   depends_on "telegraf" => :optional
   depends_on "openjdk@11" => :optional
 
   def install
-    lib.install "wavefront-proxy.jar"
-    bin.install "wfproxy"
-    etc.install "proxy.conf" => "wfproxy/proxy.conf"
-    etc.install "log4j2.xml" => "wfproxy/log4j2.xml"
-
     (etc/"wavefront/wavefront-proxy").mkpath
     (var/"spool/wavefront-proxy").mkpath
     (var/"log/wavefront").mkpath
+
+    lib.install "wavefront-proxy.jar"
+    bin.install "wfproxy" => "wfproxy_new"
+    etc.install "wavefront.conf" => "wavefront/wavefront-proxy/wavefront.conf"
+    etc.install "log4j2.xml" => "wavefront/wavefront-proxy/log4j2.xml"
   end
 
-  plist_options :manual => "wfproxy -f #{HOMEBREW_PREFIX}/etc/wavefront/wavefront-proxy/wavefront.conf"
+  plist_options :manual => "wfproxy_new"
 
-  def plist; <<-EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>KeepAlive</key>
-        <dict>
-          <key>SuccessfulExit</key>
-          <false/>
-        </dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/wfproxy</string>
-          <string>-f</string>
-          <string>#{etc}/wavefront/wavefront-proxy/wavefront.conf</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>WorkingDirectory</key>
-        <string>#{var}/spool/wavefront-proxy</string>
-        <key>StandardErrorPath</key>
-        <string>#{var}/log/wavefront/wavefront.log</string>
-        <key>StandardOutPath</key>
-        <string>#{var}/log/wavefront/wavefront.log</string>
-      </dict>
-    </plist>
-    EOS
+  service do
+    log_path var/"log/wavefront/stdout.log"
+    error_log_path var/"log/wavefront/stderr.log"
+    keep_alive true
+    run bin/"wfproxy_new"
   end
 end
