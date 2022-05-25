46f6c34188fb1dc159e7b95fbd3109a68c664b61
German Laullon <glaullon@vmware.com>
2022-05-25 12:02:28 +0200

    Update wfproxy_new.rb

--- Formula/wfproxy_new.rb
+++ Formula/wfproxy_new.rb
@@ -4,7 +4,7 @@ class WfproxyNew < Formula
   sha256 "29bcf4d0db15caca6e734f00f0cf8f7a479f4ea0c0830372210a0a5e4ab381da"
 
   depends_on "telegraf" => :optional
-  depends_on "openjdk@11" => :optional
+  depends_on "java11"
 
   def install
     (etc/"wavefront/wavefront-proxy").mkpath
