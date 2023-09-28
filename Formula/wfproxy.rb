1660c44f624195b2702a72255765ca4339714698
wf-jenkins <svc.tobs-eso@vmware.com>
2023-09-28 11:55:51 -0700

    update Formula/wfproxy.rb for wavefront-proxy-13.3.0.zip

--- Formula/wfproxy.rb
+++ Formula/wfproxy.rb
@@ -1,7 +1,7 @@
 class Wfproxy < Formula
   homepage "https://www.wavefront.com"
-  url "https://wavefront-cdn.s3.us-west-2.amazonaws.com/brew/wavefront-proxy-13.2.0.zip"
-  sha256 "93b3ab457ecf98528232d73d302f590b242643a8ac5fd90eab1f1ee604abcc9e"
+  url "https://wavefront-cdn.s3.us-west-2.amazonaws.com/brew/wavefront-proxy-13.3.0.zip"
+  sha256 "fac012780245c69f174c5a57803aa4ef6f8ba95a94bfbd773f0aa3eae88f1947"
 
   depends_on "telegraf" => :optional
   depends_on "java11" => :recommended
